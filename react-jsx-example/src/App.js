14b803211d1de6acd744d932234563d60d2608bb
JuYoung <jjystar3@gmail.com>
2024-10-29 12:09:20 +0900

    수정~

--- react-jsx-example/src/App.js
+++ react-jsx-example/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+// 수정~
+
 function App() {
 
   // jsx는 하나의 요소만 반환해야 하므로
   // 부모 태그로 자식들을 감싸야 한다
   return (
-    // 빈태그
+    // 빈태그: 부모가 필요할 때 사용할 것
     <>
       <h1>안녕하세요</h1>
       <h1>안녕하세요</h1>
